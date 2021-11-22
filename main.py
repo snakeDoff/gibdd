4d3d0386a02bf3e2ba085deed145d65f065ce5ad
snaked <vladislav.versta@yandex.ru>
2021-11-22 11:02:44 +0300

    time 22.11.21 11 02

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import Reformat as ref
 
 from PyQt5.QtWidgets import (QWidget, QSlider, QLineEdit, QLabel, QPushButton, QScrollArea, QApplication,
@@ -36,38 +35,13 @@ class Rules(QWidget):
         self.scroll.show()
 
 
-        self.scroll.setFixedSize(881, 800)
+        self.scroll.setFixedSize(881, 600)
 
-
-class Signs(QWidget):
-
+class Bilet(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        uic.loadUi('bilet.ui', self)
 
-    def initUI(self):
-        self.scroll = QScrollArea()
-        self.widget = QWidget()
-        self.vbox = QVBoxLayout()
-        self.label = QLabel()
-        self.label.setGeometry(0, 0, 854, 7048)
-        self.label.move(0, 0)
-
-
-
-        self.vbox.addWidget(self.label)
-
-        self.widget.setLayout(self.vbox)
-
-
-        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
-        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.scroll.setWidgetResizable(True)
-        self.scroll.setWidget(self.widget)
-        self.scroll.show()
-
-
-        self.scroll.setFixedSize(881, 800)
 
 class MyWidget(QMainWindow):
     def __init__(self):
@@ -75,6 +49,8 @@ class MyWidget(QMainWindow):
         uic.loadUi('main.ui', self)
         self.go2rules.clicked.connect(self.go2rule)
         self.go2signs.clicked.connect(self.go2sign)
+        self.go2dopusks.clicked.connect(self.go2dopusk)
+        self.go2bilets.clicked.connect(self.go2bilet)
 
     def go2rule(self):
         text = ref.rrules(self.PddRules.currentText())
@@ -87,10 +63,27 @@ class MyWidget(QMainWindow):
     def go2sign(self):
         text = ref.rrules(self.PddSigns.currentText())
 
-        self.w2 = Signs()
+        self.w2 = Rules()
         self.w2.scroll.setWindowTitle(text[0])
         self.w2.label.setPixmap(QPixmap(text[1]))
         self.w2.scroll.show()
+
+    def go2dopusk(self):
+        text = ref.rrules(self.PddDopusk.currentText())
+
+        self.w2 = Rules()
+        self.w2.scroll.setWindowTitle(text[0])
+        self.w2.label.setPixmap(QPixmap(text[1]))
+        self.w2.scroll.show()
+
+    def go2bilet(self):
+        self.w2 = Bilet()
+        self.w2.setWindowTitle(self.PddVariants.currentText())
+        self.w2.show()
+
+
+
+
 
 
 if __name__ == '__main__':
